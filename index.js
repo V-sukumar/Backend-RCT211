2be74689f8e27f57723f243ff2b1a30a1897d7c3
V-sukumar <thalasuku2001@gmail.com>
2023-06-14 13:07:01 +0530

    products

---
+++ index.js
@@ -0,0 +1,12 @@
+const jsonServer = require("json-server"); // importing json-server library
+const server = jsonServer.create();
+const router = jsonServer.router("db.json");
+const middlewares = jsonServer.defaults();
+const port = process.env.PORT || 4040; // you can use any port number here
+
+server.use(middlewares);
+server.use(router);
+
+server.listen(port,()=>{
+    console.log("Server is Running");
+});
